701a7816ad0c14237175f5751052f7adfc871e27
Artovnia <sayuri.platform@gmail.com>
2026-01-26 23:12:39 +0100

    hotfix

--- src/components/organisms/VendorPageBlocks/blocks/QuoteBlock.tsx
+++ src/components/organisms/VendorPageBlocks/blocks/QuoteBlock.tsx
@@ -25,7 +25,7 @@ export const QuoteBlock = ({ data }: QuoteBlockProps) => {
         <path d="M14.017 21v-7.391c0-5.704 3.731-9.57 8.983-10.609l.995 2.151c-2.432.917-3.995 3.638-3.995 5.849h4v10h-9.983zm-14.017 0v-7.391c0-5.704 3.748-9.57 9-10.609l.996 2.151c-2.433.917-3.996 3.638-3.996 5.849h3.983v10h-9.983z" />
       </svg>
       <blockquote className="text-xl md:text-2xl italic text-[#3B3634] mb-4 font-instrument-serif">
-        "{quote}"
+        &ldquo;{quote}&rdquo;
       </blockquote>
       {(author || author_title) && (
         <footer className="text-[#3B3634]/70">
